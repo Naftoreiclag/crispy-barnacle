2845ee2258bdd8b6d75aaa0d988f78a179f0467f
Naftoreiclag <naftoreiclag@gmail.com>
2016-08-09 23:38:36 -0700

    Add LICENSE
    also FFTW test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
+/*
+    Copyright 2016 James Fong
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <iostream>
+#include <string>
+#include <stdint.h>
+
+#include "fftw3.h"
+
+int run(std::string inputAudioFilename) {
+    
+    int sampleRate = 48000;
+    
+    int frameMilliseconds = 25;
+    int frameSampleSize = (frameMilliseconds * sampleRate) / 1000;
+    
+    {
+        fftw_complex* fftwInput = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * frameSampleSize);
+        fftw_complex* fftwOutput = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * frameSampleSize);
+        
+        std::cout << "Initializing FFTW... ";
+        fftw_plan fftwPlan = fftw_plan_dft_1d(frameSampleSize, fftwInput, fftwOutput, FFTW_FORWARD, FFTW_MEASURE);
+        std::cout << "done!" << std::endl;
+        
+        fftw_destroy_plan(fftwPlan);
+        fftw_free(fftwOutput);
+        fftw_free(fftwInput);
+    }
+    
+    return 0;
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello world" << std::endl;
+    
+    if(argc == 1) {
+        std::cout << "Usage: " << argv[0] << std::endl;
+        return 0;
+    } else if(argc > 1) {
+        return run(argv[1]);
+    }
+    
     return 0;
 }
