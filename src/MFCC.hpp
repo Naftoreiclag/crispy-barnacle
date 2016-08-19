fefd5b56b130126824be20c2419e69200034857c
Naftoreiclag <naftoreiclag@gmail.com>
2016-08-19 12:50:09 -0700

    Encapsulate MFCC calculation

---
+++ src/MFCC.hpp
@@ -0,0 +1,55 @@
+/*
+    Copyright 2016 James Fong
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MFCC_HPP
+#define MFCC_HPP
+
+#include <stdint.h>
+
+#include "WaveformIO.hpp"
+
+double melScale(double hertz);
+
+double invMelScale(double mels);
+
+struct MFCCParams {
+    double frameLengthMilliseconds = 25;
+    double frameStepMilliseconds = 10;
+    double filterMinFreq = 300;
+    double filterMaxFreq = 8000;
+    int32_t numFilterbanks = 26;
+    int32_t numMfccs = 12;
+};
+
+struct MFCC {
+    MFCC(double** mfccs, int32_t windowCount, int32_t mfccCount);
+    ~MFCC();
+    
+    double** data;
+    
+    int64_t numWindows;
+    int32_t numMfccs;
+    
+    MFCCParams params;
+};
+
+MFCC* generateMFCC(
+    Waveform inputAudio, 
+    const MFCCParams params,
+    bool debugOutput = false);
+
+#endif // MFCC_HPP
